9c76fd369ab9bd64e287d6aeb65c8a75abf9a1d6
rpaternina <robertpaternina65@gmail.com>
2024-12-14 15:04:39 -0500

    first commit

---
+++ src/main/java/com/robert/JobsApiApplication.java
@@ -0,0 +1,13 @@
+package com.robert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robert/JobsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
